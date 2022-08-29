4db9d0054fce32085f3f8a277e56394637f8ef21
Adrian Firmalan <afirmalan@yahoo.com>
2022-08-29 13:21:39 -0700

    corrected small issues on hmtl and api routes

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const express = require ("express")
-const htmlRoutes=requiret("./routes/htmlRoutes")
+const express = require("express")
+const htmlRoutes = require("./routes/htmlRoutes")
+const apiRoutes = require("./routes/apiRoutes")
 const PORT = process.env.PORT || 3001
 
-const app = express () 
+const app = express()
 
 app.use(express.static("public"))
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(htmlRoutes)
 
-app. listen(PORT,()=>{
+app.use(apiRoutes)
+
+
+
+
+
+app.listen(PORT, () => {
     console.log("App is listening on Port http://localhost:" + PORT)
 })
